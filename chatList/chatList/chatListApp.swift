44079d895331bc9da4cc85f28e2d68216f3e8312
serjIvchenko <Ti4wpRWub2mw2Lm>
2024-08-30 17:48:14 +0300

    added realizations for view chat messages

---
+++ chatList/chatList/chatListApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatListApp.swift
+//  chatList
+//
+//  Created by Sergey Ivchenko on 30.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct chatListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
